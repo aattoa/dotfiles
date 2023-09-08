36144edec0b0cdf9af928f089ad9fb8db7d3a2f9
aattoa <aattoa@proton.me>
2023-09-09 02:32:22 +0300

    Improve neovim config

--- configs/neovim/lua/mystuff/mappings.lua
+++ configs/neovim/lua/mystuff/mappings.lua
@@ -6,20 +6,13 @@ vim.keymap.set("n", "<leader>h", ":set hlsearch!<return>")
 -- Toggle line numbers
 vim.keymap.set("n", "<leader>n", ":set number!<return>:set relativenumber!<return>")
 
--- Try to keep the cursor centered
-vim.keymap.set({ "n", "v" }, "G",     "Gzz")
-vim.keymap.set({ "n", "v" }, "n",     "nzz")
-vim.keymap.set({ "n", "v" }, "N",     "Nzz")
-vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
-vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
-vim.keymap.set({ "n", "v" }, "{",     "{zz")
-vim.keymap.set({ "n", "v" }, "}",     "}zz")
-
 -- Tab controls
 vim.keymap.set("n", "<C-t>",   vim.cmd.tabnew)
 vim.keymap.set("n", "<C-q>",   vim.cmd.tabclose)
 vim.keymap.set("n", "<Tab>",   vim.cmd.tabnext)
 vim.keymap.set("n", "<S-Tab>", vim.cmd.tabprevious)
+vim.keymap.set("n", "g<",      ":tabmove -1<return>")
+vim.keymap.set("n", "g>",      ":tabmove +1<return>")
 
 -- https://vim.fandom.com/wiki/Moving_lines_up_or_down
 vim.keymap.set("n", "<C-k>", ":m .-2<return>==") -- Current line up
@@ -38,11 +31,21 @@ vim.keymap.set({ "n", "v" }, "\"", "10kzz")
 vim.keymap.set({ "n", "v" }, "|",  "10jzz")
 
 -- System clipboard
-vim.keymap.set({ "n", "v" }, "<C-y>", "\"+y")
-vim.keymap.set({ "n", "v" }, "<C-p>", "\"+p")
+vim.keymap.set({ "n", "v" }, "Y", "\"+y")
+vim.keymap.set({ "n", "v" }, "P", "\"+p")
 
 -- Disable arrow keys
 vim.keymap.set({ "n", "v", "i" }, "<Up>",    "")
 vim.keymap.set({ "n", "v", "i" }, "<Down>",  "")
 vim.keymap.set({ "n", "v", "i" }, "<Left>",  "")
 vim.keymap.set({ "n", "v", "i" }, "<Right>", "")
+
+---@param movements table
+local function center_cursor_after_movements (movements)
+    for _, movement in ipairs(movements) do
+        vim.keymap.set({ "n", "v" }, movement, movement.."zz")
+    end
+end
+
+-- Try to keep the cursor centered
+center_cursor_after_movements { "G", "n", "N", "<C-d>", "<C-u>", "{", "}" }
