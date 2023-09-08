d5415058afb61629e969ce98cdc9b972c5643732
aattoa <aattoa@proton.me>
2023-09-09 01:21:41 +0300

    Update neovim config

--- configs/neovim/lua/mystuff/mappings.lua
+++ configs/neovim/lua/mystuff/mappings.lua
@@ -1,41 +1,48 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+-- Explore with Netrw
+vim.keymap.set("n", "<leader>e", vim.cmd.Explore)
 
 -- Toggle search highlight
 vim.keymap.set("n", "<leader>h", ":set hlsearch!<return>")
 -- Toggle line numbers
-vim.keymap.set("n", "<leader>n", ":set number!<return>")
+vim.keymap.set("n", "<leader>n", ":set number!<return>:set relativenumber!<return>")
 
 -- Try to keep the cursor centered
-vim.keymap.set("n", "G",     "Gzz")
-vim.keymap.set("n", "n",     "nzz")
-vim.keymap.set("n", "N",     "Nzz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "{",     "{zz")
-vim.keymap.set("n", "}",     "}zz")
+vim.keymap.set({ "n", "v" }, "G",     "Gzz")
+vim.keymap.set({ "n", "v" }, "n",     "nzz")
+vim.keymap.set({ "n", "v" }, "N",     "Nzz")
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
+vim.keymap.set({ "n", "v" }, "{",     "{zz")
+vim.keymap.set({ "n", "v" }, "}",     "}zz")
+
+-- Tab controls
+vim.keymap.set("n", "<C-t>",   vim.cmd.tabnew)
+vim.keymap.set("n", "<C-q>",   vim.cmd.tabclose)
+vim.keymap.set("n", "<Tab>",   vim.cmd.tabnext)
+vim.keymap.set("n", "<S-Tab>", vim.cmd.tabprevious)
 
 -- https://vim.fandom.com/wiki/Moving_lines_up_or_down
-vim.keymap.set("n", "<C-k>", ":m .-2<return>==") -- Single line up
-vim.keymap.set("n", "<C-j>", ":m .+1<return>==") -- Single line down
+vim.keymap.set("n", "<C-k>", ":m .-2<return>==") -- Current line up
+vim.keymap.set("n", "<C-j>", ":m .+1<return>==") -- Current line down
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv") -- Selected lines up
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv") -- Selected lines down
 
+-- Search and replace
+vim.keymap.set("n", "<C-s>", ":%s//g<Left><Left>")
+
+-- Normal mode on selected lines
+vim.keymap.set("v", "<C-n>", ":normal ")
+
 -- Movement
-vim.keymap.set("n", "\"", "10kzz")
-vim.keymap.set("v", "\"", "10kzz")
-vim.keymap.set("n", "|",  "10jzz")
-vim.keymap.set("v", "|",  "10jzz")
+vim.keymap.set({ "n", "v" }, "\"", "10kzz")
+vim.keymap.set({ "n", "v" }, "|",  "10jzz")
+
+-- System clipboard
+vim.keymap.set({ "n", "v" }, "<C-y>", "\"+y")
+vim.keymap.set({ "n", "v" }, "<C-p>", "\"+p")
 
 -- Disable arrow keys
-vim.keymap.set("n", "<Up>",    "")
-vim.keymap.set("v", "<Up>",    "")
-vim.keymap.set("i", "<Up>",    "")
-vim.keymap.set("n", "<Down>",  "")
-vim.keymap.set("v", "<Down>",  "")
-vim.keymap.set("i", "<Down>",  "")
-vim.keymap.set("n", "<Left>",  "")
-vim.keymap.set("v", "<Left>",  "")
-vim.keymap.set("i", "<Left>",  "")
-vim.keymap.set("n", "<Right>", "")
-vim.keymap.set("v", "<Right>", "")
-vim.keymap.set("i", "<Right>", "")
+vim.keymap.set({ "n", "v", "i" }, "<Up>",    "")
+vim.keymap.set({ "n", "v", "i" }, "<Down>",  "")
+vim.keymap.set({ "n", "v", "i" }, "<Left>",  "")
+vim.keymap.set({ "n", "v", "i" }, "<Right>", "")
--- configs/neovim/lua/mystuff/settings.lua
+++ configs/neovim/lua/mystuff/settings.lua
@@ -1,30 +1,31 @@
-vim.g.mapleader       = " "             -- Set the leader key
+vim.g.mapleader          = " "             -- Set the leader key
 
-vim.g.netrw_banner    = 0               -- Disable the Netrw help banner
-vim.g.netrw_list_hide = "^\\.\\/$"      -- Disable current directory entry
+vim.g.netrw_banner       = 0               -- Disable the Netrw help banner
+vim.g.netrw_list_hide    = "^\\.\\/$"      -- Disable current directory entry
 
-vim.opt.guicursor     = ""              -- Disable cursor styling
-vim.opt.cursorline    = true            -- Highlight current line
+vim.opt.guicursor        = ""              -- Disable cursor styling
+vim.opt.cursorline       = true            -- Highlight current line
 
-vim.opt.number        = true            -- Enable line numbers
+vim.opt.relativenumber   = true            -- Enable relative line numbers
+vim.opt.number           = true            -- Absolute line number for current line
 
-vim.opt.tabstop       = 4               -- Tab character width
-vim.opt.expandtab     = true            -- Insert spaces in place of tabs
+vim.opt.tabstop          = 4               -- Tab character width
+vim.opt.expandtab        = true            -- Insert spaces in place of tabs
 
-vim.opt.shiftwidth    = 4               -- Indentation width
-vim.opt.smartindent   = true            -- Automatically indent new lines
+vim.opt.shiftwidth       = 4               -- Indentation width
+vim.opt.smartindent      = true            -- Automatically indent new lines
 
-vim.opt.ignorecase    = true            -- Enable case insensitive search
-vim.opt.hlsearch      = false           -- Do not highlight search matches by default
-vim.opt.incsearch     = true            -- Interactive search highlight
+vim.opt.ignorecase       = true            -- Enable case insensitive search
+vim.opt.hlsearch         = true            -- Highlight search matches by default
+vim.opt.incsearch        = true            -- Interactive search highlight
 
-vim.opt.updatetime    = 100             -- Faster CursorHold events
+vim.opt.updatetime       = 100             -- Faster CursorHold events
 
-vim.opt.undofile      = true            -- Enable persistent undo
+vim.opt.undofile         = true            -- Enable persistent undo
 
-vim.opt.wrap          = false           -- Do not wrap long lines
+vim.opt.wrap             = false           -- Do not wrap long lines
 
-vim.opt.pumheight     = 10              -- Limit autocompletion suggestions
+vim.opt.pumheight        = 10              -- Limit autocompletion suggestions
 
-vim.opt.scrolloff     = 10              -- Vertical scrolloff
-vim.opt.sidescrolloff = 20              -- Horizontal scrolloff
+vim.opt.scrolloff        = 10              -- Vertical scrolloff
+vim.opt.sidescrolloff    = 20              -- Horizontal scrolloff
